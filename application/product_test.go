c54ff861f277af43d54e889c08ce5ee635c50248
eduardo3g <eduardosbrasil10@gmail.com>
2021-05-08 19:06:37 -0300

    test: implement unit test to validate the IsValid function of the Product interface

--- application/product_test.go
+++ application/product_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"github.com/codeedu/go-hexagonal/application"
+	uuid "github.com/satori/go.uuid"
 	"github.com/stretchr/testify/require"
 )
 
@@ -33,4 +34,27 @@ func TestProduct_Disable(t *testing.T) {
 	product.Price = 10
 	err = product.Disable()
 	require.Equal(t, "the price must be 0 in order to have the product disabled", err.Error())
+}
+
+func TestProduct_IsValid(t *testing.T) {
+	product := application.Product{}
+	product.ID = uuid.NewV4().String()
+	product.Name = "Hello"
+	product.Status = application.DISABLED
+	product.Price = 10
+
+	_, err := product.IsValid()
+	require.Nil(t, err)
+
+	product.Status = "INVALID"
+	_, err = product.IsValid()
+	require.Equal(t, "the status must be enabled or disabled", err.Error())
+
+	product.Status = application.ENABLED
+	_, err = product.IsValid()
+	require.Nil(t, err)
+
+	product.Price = -10
+	_, err = product.IsValid()
+	require.Equal(t, "the price must be greater or equal to 0", err.Error())
 }

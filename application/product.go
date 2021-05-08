93379ab8fa5fdac8dc3a38027a09d1645a6b1b33
eduardo3g <eduardosbrasil10@gmail.com>
2021-05-08 18:26:12 -0300

    feat: implement GetId, GetName, GetStatus and GetPrice functions inside Product interface

--- application/product.go
+++ application/product.go
@@ -28,10 +28,18 @@ func (p *Product) Enable() error {}
 
 func (p *Product) Disable() error {}
 
-func (p *Product) GetId() string {}
+func (p *Product) GetId() string {
+	return p.ID
+}
 
-func (p *Product) GetName() string {}
+func (p *Product) GetName() string {
+	return p.Name
+}
 
-func (p *Product) GetStatus() string {}
+func (p *Product) GetStatus() string {
+	return p.Status
+}
 
-func (p *Product) GetPrice() float32 {}
+func (p *Product) GetPrice() float32 {
+	return p.Price
+}

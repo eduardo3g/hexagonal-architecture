7fd42d6466dadedfc26267932fd2c6e87864b838
eduardo3g <eduardosbrasil10@gmail.com>
2021-05-08 18:46:39 -0300

    feat: implement the Disable function of the Product interface

--- application/product.go
+++ application/product.go
@@ -35,7 +35,14 @@ func (p *Product) Enable() error {
 	return errors.New("the price must be greater than 0 to enable the product")
 }
 
-// func (p *Product) Disable() error {}
+func (p *Product) Disable() error {
+	if p.Price == 0 {
+		p.Status = DISABLED
+		return nil
+	}
+
+	return errors.New("the price must be 0 in order to have the product disabled")
+}
 
 func (p *Product) GetId() string {
 	return p.ID

88e1bef5d91f877fe72e223ea75b76d66fb1a813
eduardo3g <eduardosbrasil10@gmail.com>
2021-05-11 19:57:56 -0300

    feat: implement the Create method on the Product Service

--- application/product_service.go
+++ application/product_service.go
@@ -13,3 +13,22 @@ func (s *ProductService) Get(id string) (ProductInterface, error) {
 
 	return product, nil
 }
+
+func (s *ProductService) Create(name string, price float32) (ProductInterface, error) {
+	product := NewProduct()
+	product.Name = name
+	product.Price = price
+	_, err := product.IsValid()
+
+	if err != nil {
+		return &Product{}, err
+	}
+
+	result, err := s.Persistence.Save(product)
+
+	if err != nil {
+		return &Product{}, err
+	}
+
+	return result, nil
+}
